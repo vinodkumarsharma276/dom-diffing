2d50a58d4d49377ba7c5b58c3db504c160c87895
Vinod Kumar Sharma <vinodsharma@microsoft.com>
2022-03-20 15:40:52 +0530

    Created parent links for nodes

--- src/parseDomPlaywright.js
+++ src/parseDomPlaywright.js
@@ -1,48 +1,73 @@
 import playwright from "playwright";
 import * as fs from "fs";
 
-const script = () => {
+const parseHTMLAndKeepRelations = () => {
     let nodes = [];
     const allPageElements = document.querySelectorAll("*");
     
-    for(var j=0; j<allPageElements.length; j++){
-        let node = {};
+    for(let i=0; i<allPageElements.length; i++){
+        let element = allPageElements[i];
+        element["visited"] = false;
+        element["visitedChild"] = false;
+        element["id"] = i;
+        element["parentId"] = -1;
+    }
+
+    for(let j=0; j<allPageElements.length; j++){
         let element = allPageElements[j];
 
-        node["tag"] = element.tagName;
-        
-        if(element.hasAttributes()){
-            const attributes = element.attributes;
-            const appliedCSS = window.getComputedStyle(element);
-            const attrsValue = {};
-            const style = {};
+        if(!element["visited"]){
+            let node = {};
+            element["visited"] = true;
+            node["tag"] = element.tagName;
+            node["id"] = element["id"];
+            node["parentId"] = element["parentId"];
 
-            node["attributes"] = attrsValue;
-            node["css"] = style;
-
-            for(var i=0; i<attributes.length; i++){
-                attrsValue[attributes[i].name] = attributes[i].value;
+            if(element.hasAttributes()){
+                const attributes = element.attributes;
+                const attrsValue = {};
+    
+                node["attributes"] = attrsValue;
+    
+                for(let i=0; i<attributes.length; i++){
+                    attrsValue[attributes[i].name] = attributes[i].value;
+                }    
             }
 
-            //Calculate Style
-            for(var i=0; i<appliedCSS.length; i++){
+            const appliedCSS = window.getComputedStyle(element);
+            const style = {};
+            node["css"] = style;
+
+            for(let i=0; i<appliedCSS.length; i++){
                 var propName = appliedCSS.item(i);
 
                 style[propName] = appliedCSS.getPropertyValue(propName);
             }
+
+            element["visited"] = true;
+            nodes[j] = node;
         }
-        nodes[j] = node;
+
+        if(!element["visitedChild"]){
+            for(let k=0; k<element.childNodes.length; k++){
+                let child = element.childNodes[k];
+                child["parentId"] = element["id"];
+                console.log(`setting parent id: ${child}`);
+            }
+            element["visitedChild"] = true;
+        }
     }
 
     return nodes;
 }
 
+
 const main = async() => {
     const browser = await playwright.chromium.launch({headless: false});
     const page = await browser.newPage();
     await page.goto("https://www.google.com");
-    
-    const result = await page.evaluate(script);
+    await page.exposeFunction("iterateElement", iterateElement);
+    const result = await page.evaluate(parseHTMLAndKeepRelations);
 
     // console.log(`element: ${JSON.stringify(result, null, 2)}`);
 
